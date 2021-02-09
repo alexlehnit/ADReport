a74e62a97e6196b1957322ff1ef35f90dcecf937
alexlehnit <67313267+alexlehnit@users.noreply.github.com>
2021-02-09 14:35:48 +0100

    Beginn mit Excel Export Tool
    Excel Export ist möglich
    nur muss man noch die Daten Übergabe von dem AD Tool anpassen heißt das der Data tabel auch die Daten auffassen kann

--- ADReport/ADReport/Form1.cs
+++ ADReport/ADReport/Form1.cs
@@ -4,17 +4,110 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using DataTable = System.Data.DataTable;
 
 namespace ADReport
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+                var userList = UserManager.SeedData();
+                try
+                {
+                    Console.WriteLine("Please select a operation to do ..");
+                    Console.WriteLine("1. Export Data as EXCEL");
+                
+                    int command = Convert.ToInt32(Console.ReadLine());
+                    
+                            string fileName = "UserManager.xlsx";
+                            Console.WriteLine("Please give a location to save :");
+                            string location = @"C:\tmp";
+                            string customExcelSavingPath = location + "\\" + fileName;
+                            GenerateExcel(ConvertToDataTable(userList), customExcelSavingPath);
+                      
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            
+
+        }
+        // T is a generic class
+        static DataTable ConvertToDataTable<T>(List<T> models)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            // creating a data table instance and typed it as our incoming model 
+            // as I make it generic, if you want, you can make it the model typed you want. DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties of that model
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            // Loop through all the properties            
+            // Adding Column name to our datatable
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names  
+                dataTable.Columns.Add(prop.Name);
+            }
+            // Adding Row and its value to our dataTable
+            foreach (T item in models)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows  
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                // Finally add value to datatable  
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+        public static void GenerateExcel(DataTable dataTable, string path)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add(dataTable);
+            // create a excel app along side with workbook and worksheet and give a name to it
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWorkBook = excelApp.Workbooks.Add();
+            Excel._Worksheet xlWorksheet = excelWorkBook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            foreach (DataTable table in dataSet.Tables)
+            {
+                //Add a new worksheet to workbook with the Datatable name
+                Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
+                excelWorkSheet.Name = table.TableName;
+                // add all the columns
+                for (int i = 1; i < table.Columns.Count + 1; i++)
+                {
+                    excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                }
+                // add all the rows
+                for (int j = 0; j < table.Rows.Count; j++)
+                {
+                    for (int k = 0; k < table.Columns.Count; k++)
+                    {
+                        excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                    }
+                }
+            }
+            // excelWorkBook.Save(); -> this will save to its default location
+            excelWorkBook.SaveAs(path); // -> this will do the custom
+            excelWorkBook.Close();
+            excelApp.Quit();
+        }
     }
 }
+
